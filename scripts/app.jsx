04f077329475d97ec83d42c1dd836f9ba44572ae
Dmitry Starostin <incrop@yandex-team.ru>
2015-07-09 22:23:54 +0300

    Create helper for generating scenes

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -18,7 +18,6 @@ var SvgGraph = React.createClass({
     },
     componentWillMount: function() {
         this.updateDimensions();
-
     },
     componentDidMount: function() {
         window.addEventListener('resize', this.updateDimensions);
@@ -26,18 +25,22 @@ var SvgGraph = React.createClass({
     componentWillUnmount: function() {
         window.removeEventListener('resize', this.updateDimensions);
     },
+    componentDidUpdate: function (oldProps) {
+        var oldFrame = oldProps.frame,
+            newFrame = this.props.frame;
+        if (oldFrame.width != newFrame.width || oldFrame.height != newFrame.height) {
+            this.updateDimensions();
+        }
+    },
     calcLinkPath: function(id, link) {
         var nodes = this.props.frame.nodes,
             lineCoords = id.split('_').map(function(nodeId) {
                 var node = nodes[nodeId];
                 return [node.x, node.y];
             });
-        var through = !link.through || link.through.length === 0 ? []
-            : typeof link.through[0] !== 'object' ? [link.through]
-            : link.through;
-        Array.prototype.splice.apply(lineCoords, [1, 0].concat(through));
+        Array.prototype.splice.apply(lineCoords, [1, 0].concat(link.through || []));
         return 'M' + lineCoords.map(function(point) {
-            return (point.length === 3 ? point[2] + ' ' : '') +
+            return typeof point === 'string' ? point :
                 ((point[0] + 1) * this.state.scaleX) + ',' +
                 ((point[1] + 1) * this.state.scaleY);
         }.bind(this)).join(' ');
@@ -88,36 +91,36 @@ var KEY_LEFT = 37,
 
 var Slides = React.createClass({
     componentWillMount: function() {
+        var frameIdxs = [];
+        for (var i = 0; i < this.props.scenes.length; i++) frameIdxs.push(0);
         this.setState({
-            sceneIdx: 0,
-            frameIdx: 0
+            frameIdxs: frameIdxs,
+            sceneIdx: 0
         });
     },
     handleKey: function(e) {
-        var s = this.state.sceneIdx,
-            f = this.state.frameIdx,
+        var frameIdxs = this.state.frameIdxs,
+            sceneIdx = this.state.sceneIdx,
             scenes = this.props.scenes;
         switch (e.keyCode) {
             case KEY_LEFT:
-                if (f > 0) {
-                    f--;
-                } else if (s > 0) {
-                    f = scenes[--s].length - 1;
-                }
+                if (frameIdxs[sceneIdx] > 0) frameIdxs[sceneIdx]--;
                 break;
             case KEY_RIGHT:
-                if (f < scenes[s].length - 1) {
-                    f++;
-                } else if (s < scenes.length - 1){
-                    f = 0;
-                    s++;
-                }
+                if (frameIdxs[sceneIdx] < scenes[sceneIdx].length - 1) frameIdxs[sceneIdx]++;
                 break;
-            //TODO: UP, DOWN
+            case KEY_UP:
+                if (sceneIdx > 0) sceneIdx--;
+                break;
+            case KEY_DOWN:
+                if (sceneIdx < scenes.length - 1) sceneIdx++;
+                break;
+            default:
+                return;
         }
         this.setState({
-            sceneIdx: s,
-            frameIdx: f
+            sceneIdx: sceneIdx,
+            frameIdx: frameIdxs
         });
     },
     componentDidMount: function() {
@@ -140,39 +143,131 @@ var Slides = React.createClass({
         return dest;
     },
     render: function () {
-        var scene = this.props.scenes[this.state.sceneIdx];
-        var frame = {};
-        for (var i = 0; i <= this.state.frameIdx; i++) {
+        var scene = this.props.scenes[this.state.sceneIdx],
+            framesCnt = this.state.frameIdxs[this.state.sceneIdx],
+            frame = {};
+        for (var i = 0; i <= framesCnt; i++) {
             frame = this.deepMerge(frame, scene[i]);
         }
         return <SvgGraph frame={frame} colors={['', CURR, PATH]} />
     }
 });
 
-var CURR = 'mediumorchid',
+var CURR = 'deeppink',
     PATH = 'lightseagreen',
-    DONE = 'palegoldenrod';
+    DONE = 'yellowgreen';
 
-var scenes = [[
+function Scene(initFrame, initNode) {
+    initFrame.nodes[initNode].stroke = CURR;
+    var path = [initNode],
+        frames = [initFrame];
+    function move(a, b, isFwd) {
+        if (isFwd) {
+            path.push(b);
+        } else {
+            path.pop();
+        }
+        var frame = {links: {}, nodes: {}};
+        if (a.indexOf('_') > -1) {
+            a = frame.links[a] = {};
+            b = frame.nodes[b] = {};
+        } else {
+            a = frame.nodes[a] = {};
+            b = frame.links[b] = {};
+        }
+        a.stroke = isFwd ? PATH : '';
+        b.stroke = CURR;
+        frames.push(frame);
+    }
+    return {
+        walk: function() {
+            var curr = path[path.length - 1], step, middle;
+            for (var i = 0; i < arguments.length; i++) {
+                step = arguments[i];
+                if (curr.indexOf('_') > -1) {
+                    middle = curr.split('_')[1];
+                    move(curr, middle, true);
+                    if (step.indexOf('_') > -1) {
+                        move(middle, step, true);
+                    }
+                } else {
+                    middle = curr;
+                    if (step.indexOf('_') === -1) {
+                        middle = curr + '_' + step;
+                        move(curr, middle, true);
+                    }
+                    move(middle, step, true);
+                }
+                curr = step;
+            }
+            return this;
+        },
+        backTo: function(step) {
+            var i = path.length - 1;
+            while (path[i] !== step) {
+                var last = path[i--];
+                move(last, path[i], false);
+            }
+            return this;
+        },
+        mark: function() {
+            var frame = {nodes: {}};
+            frame.nodes[path[path.length - 1]] = {fill: DONE};
+            frames.push(frame);
+            return this;
+        },
+        frames: function() {
+            return frames;
+        }
+    }
+}
 
-    {
+var scenes = [
+    Scene({
+        width: 4,
+        height: 2,
+        nodes: {
+            A: {x: 0, y: 1},
+            B: {x: 1, y: 1},
+            C: {x: 2, y: 0},
+            D: {x: 2, y: 1},
+            E: {x: 3, y: 0},
+            F: {x: 3, y: 1}
+        },
+        links: {
+            A_B: {}, B_C: {through: ['C', [2, 1], [2, 0.5]]},
+            A_C: {dashed: true},
+            A_D: {dashed: true, through: ['Q', [1, 2]]},
+            D_E: {}, D_F: {}
+        }
+    }, 'A')
+        .walk('B', 'C').mark()
+        .backTo('B').mark()
+        .backTo('A').mark()
+        .walk('A_C').backTo('A')
+        .walk('D', 'E').mark()
+        .backTo('D').walk('F').mark()
+        .backTo('D').mark()
+        .backTo('A')
+        .frames(),
+    Scene({
         width: 2,
         height: 2,
         nodes: {
-            A: {x: 0, y: 0, stroke: CURR},
+            A: {x: 0, y: 0},
             B: {x: 1, y: 0},
             C: {x: 0, y: 1}
         },
         links: {
             A_B: {},
-            B_C: {dashed: true, through: [1, 1, 'Q']},
+            B_C: {dashed: true, through: ['Q', [1, 1]]},
             C_A: {}
         }
-    },
-    {links: {A_B: {stroke: CURR}}, nodes: {A: {stroke: PATH}}},
-    {links: {A_B: {stroke: PATH}}, nodes: {B: {stroke: CURR}}},
-    {nodes: {B: {fill: DONE}}},
-    {links: {B_C: {stroke: PATH}}, nodes: {B: {stroke: CURR}}},
-]];
+    }, 'A')
+        .walk('B').mark()
+        .walk('C', 'C_A').backTo('C').mark()
+        .backTo('A').mark()
+        .frames()
+];
 
 React.render(<Slides scenes={scenes} />, document.getElementById('react'));
